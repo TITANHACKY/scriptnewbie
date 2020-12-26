0a40afd4aa169fa9f176e911a5cf70f4e96fbbe8
TITAN HACKY <poonkawin8484@gmail.com>
2020-12-27 00:16:47 +0530

    added livegram.html

--- bot.py
+++ bot.py
@@ -4,9 +4,10 @@ import json
 class telegram_bot():
     def __init__(self):
         self.token = '1148896097:AAGupgauA433zltzx9F4E267z7BRq7MdJg4'
+        self.chat_id = 1037166796
         self.base_url = f"https://api.telegram.org/bot{self.token}"
 
-    #update function to get update from the bot
+    #getting updates
     def get_updates(self, offset=None):
         url = self.base_url+"/getUpdates?timeout=100"
         if offset:
@@ -14,9 +15,14 @@ class telegram_bot():
         r=requests.get(url)
         return json.loads(r.content)
 
-    #send message function to send message to user
+    #forwarding the message
+    def forward_message(self, from_chat_id, msgid):
+        url = self.base_url+f"/forwardMessage?chat_id={self.chat_id}&from_chat_id={from_chat_id}&message_id={msgid}"
+        requests.get(url)
+
     def send_messages(self, msg, chat_id):
         url = self.base_url+f"/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=Markdown"
+        print(url)
         requests.get(url)
 
     #to send photo to the user:
@@ -41,25 +47,35 @@ class telegram_bot():
                 for msgs in updates:
                     update_id = msgs["update_id"]
                     try:
-                        user_id = msgs["message"]["chat"]["id"]
-                        message = msgs["message"]["text"]
-                        telegram_bot().send_messages(message, user_id)
-                    except:
-                        try:
-                            photoid = msgs["message"]['phtot']['0']['file_id']
-                            telegram_bot().send_photo(photoid, user_id)
-                        except:
+                        from_chat_id = msgs["message"]["chat"]["id"]
+                        msgid = msgs["message"]["message_id"]
+                        chat_id = self.chat_id
+                        if(from_chat_id==chat_id):
                             try:
-                                videoid = msgs['message']['video']['file_id']
-                                telegram_bot().send_video(videoid, user_id)
+                                message = msgs["message"]["text"]
+                                reply_to_chatid = msgs["message"]["reply_to_message"]["forward_from"]["id"]
+                                telegram_bot().send_messages(message, reply_to_chatid)
                             except:
                                 try:
-                                    documentid = msgs['message']['document']['file_id']
-                                    telegram_bot().send_documents(documentid, user_id)
+                                    reply_to_chatid = msgs["message"]["reply_to_message"]["forward_from"]["id"]
+                                    photoid = msgs["message"]['photo']['0']['file_id']
+                                    telegram_bot().send_photo(photoid, reply_to_chatid)
                                 except:
-                                    print("Error")
+                                    try:
+                                        reply_to_chatid = msgs["message"]["reply_to_message"]["forward_from"]["id"]
+                                        videoid = msgs['message']['video']['file_id']
+                                        telegram_bot().send_video(videoid, reply_to_chatid)
+                                    except:
+                                        try:
+                                            reply_to_chatid = msgs["message"]["reply_to_message"]["forward_from"]["id"]
+                                            documentid = msgs['message']['document']['file_id']
+                                            telegram_bot().send_documents(documentid, reply_to_chatid)
+                                        except:
+                                            message=''
+                        else:
+                            telegram_bot().forward_message(from_chat_id,msgid)
+                    except:
+                        print('error')
 
 if __name__ == "__main__":
     telegram_bot().updates()
-
-
